fb74c026fd587d4a9d9a93fe8633a00bf796b762
Aditya Mishra <adityamishra2217@gmail.com>
2023-07-19 17:07:06 +0530

    Added code for higher order functions

---
+++ KotlinFirst/app/src/main/java/com/aditya/higher_order_functions/Discount.kt
@@ -0,0 +1,19 @@
+package com.aditya.higher_order_functions
+
+import kotlin.random.Random
+
+inline fun repeat(times: Int, action: (Int) -> Unit) {
+    for (i in 1..times) {
+        action(i)
+    }
+}
+
+fun main() {
+    var maxDiscount = 0 // Corrected: Use 'var' instead of 'val' since we are modifying it later
+    repeat(3) { repetition ->
+        val discount = Random.nextInt(10)
+        println("Attempt $repetition, discount: $discount")
+        maxDiscount = discount.takeIf { it > maxDiscount } ?: maxDiscount
+    }
+    println("Max Discount: $maxDiscount")
+}
